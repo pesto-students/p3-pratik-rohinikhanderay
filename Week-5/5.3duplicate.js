1e28032005f7fa43593da75c4db34cc97a14838e
rohinikhanderay <87316359+rohinikhanderay@users.noreply.github.com>
2022-07-16 18:34:51 +0530

    Revert "Week5"

--- Week-5/5.3duplicate.js
+++
@@ -1,9 +0,0 @@
-function hasDuplicate (arr) {
-    return arr.length === new Set(arr).size;
-  }
-  
-  let uniqueArray = [1, 2, 3, 4, 5,6,7,8];
-  console.log(`${uniqueArray} is unique : ${hasDuplicate (uniqueArray)}`);
-  
-  let nonUniqueArray = [1, 1, 2, 3, 4, 5,5];
-  console.log(`${nonUniqueArray} is unique : ${hasDuplicate (nonUniqueArray)}`);
